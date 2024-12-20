982cf005bceeca9b786895108ff168d41e25620f
Сергей <gvozd0770@gmail.com>
2024-12-20 17:43:36 +0200

    Update app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,16 @@
 package app;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int count = 1;
+
+         for (int num = 1; num <= 6; num++){
+             sum += num;
+             System.out.println(count + ") Num is " + num + ", sum is " + sum);
+             count++;
+         }
+    }
     
 }
