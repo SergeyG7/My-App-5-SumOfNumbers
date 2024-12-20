f866a950e032568509ebc64ac6bdd2361fbe954a
Сергей <gvozd0770@gmail.com>
2024-12-20 17:46:27 +0200

    Final

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,6 +11,8 @@ public class Main {
              System.out.println(count + ") Num is " + num + ", sum is " + sum);
              count++;
          }
+        System.out.println("----------------------");
+        System.out.println("Sum of numbers: " + sum);
     }
     
 }
